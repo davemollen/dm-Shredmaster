68f075775602aadc4fc1ee8e22dd7d997db6f11d
Dave Mollen <dave.mollen@infi.nl>
2024-06-05 21:33:30 +0200

    Use constants for contour

--- shredmaster/src/contour.rs
+++ shredmaster/src/contour.rs
@@ -2,12 +2,32 @@ use crate::shared::{
   bilinear_transform::BilinearTransform, third_order_iir_filter::ThirdOrderIIRFilter,
 };
 
+const R1: f32 = 100.;
+const R2: f32 = 33000.;
+const R3: f32 = 33000.;
+const R4: f32 = 100000.;
+const C1: f32 = 1e-9;
+const C2: f32 = 1e-7;
+const C3: f32 = 4.7e-8;
+
 pub struct Contour {
   filter: ThirdOrderIIRFilter,
   bilinear_transform: BilinearTransform,
 }
 
 impl Contour {
+  const C1C2: f32 = C1 * C2;
+  const C1C3: f32 = C1 * C3;
+  const C2C3: f32 = C2 * C3;
+  const C1C3R1: f32 = Self::C1C3 * R1;
+  const C1C2C3: f32 = Self::C1C2 * C3;
+  const C1C3R2: f32 = Self::C1C3 * R2;
+  const C2C3R1: f32 = Self::C2C3 * R1;
+  const C1C2R1: f32 = Self::C1C2 * R1;
+  const C1C2C3R1: f32 = Self::C1C2C3 * R1;
+  const C1C2C3R1R3: f32 = Self::C1C2C3R1 * R3;
+  const C1C2C3R1R2: f32 = Self::C1C2C3R1 * R2;
+
   pub fn new(sample_rate: f32) -> Self {
     Self {
       bilinear_transform: BilinearTransform::new(sample_rate),
@@ -22,55 +42,38 @@ impl Contour {
   }
 
   fn get_s_domain_coefficients(&self, contour: f32) -> ([f32; 4], [f32; 4]) {
-    let r1 = 100.;
-    let r2 = 33000.;
-    let r3 = 33000.;
-    let r4 = 100000.;
-    let r4_a = contour * r4;
-    let r4_b = (1. - contour) * r4;
-    let c1 = 1e-9;
-    let c2 = 1e-7;
-    let c3 = 4.7e-8;
+    let r4_a = contour * R4;
+    let r4_b = (1. - contour) * R4;
 
-    let c1c2 = c1 * c2;
-    let c1c3 = c1 * c3;
-    let c2c3 = c2 * c3;
-    let c1c3r1 = c1c3 * r1;
-    let c1c2c3 = c1c2 * c3;
-    let c1c3r2 = c1c3 * r2;
-    let c2c3r1 = c2c3 * r1;
-    let c1c2r1 = c1c2 * r1;
-    let c1c2c3r1 = c1c2c3 * r1;
-    let c1c2c3r1r3 = c1c2c3r1 * r3;
-    let c1c2c3r1r2 = c1c2c3r1 * r2;
-    let r3r4_a = r3 * r4_a;
-    let r2r4_a = r2 * r4_a;
+    let r3r4_a = R3 * r4_a;
+    let r2r4_a = R2 * r4_a;
 
-    let b0 = r4_b * c1c2c3 * r3 * r4_a + r4_b * c1c2c3 * r2r4_a + c1c2c3 * r2r4_a * r3;
-    let b1 = r4_b * c2c3 * r4_a
-      + r4_b * c1c3 * r3
-      + c1c2 * r3r4_a
-      + r4_b * c1c3r2
-      + c1c2 * r2r4_a
-      + c1c3r2 * r3;
-    let b2 = r4_b * c3 + c2 * r4_a + c1 * r3 + c1 * r2;
+    let b0 =
+      r4_b * Self::C1C2C3 * R3 * r4_a + r4_b * Self::C1C2C3 * r2r4_a + Self::C1C2C3 * r2r4_a * R3;
+    let b1 = r4_b * Self::C2C3 * r4_a
+      + r4_b * Self::C1C3 * R3
+      + Self::C1C2 * r3r4_a
+      + r4_b * Self::C1C3R2
+      + Self::C1C2 * r2r4_a
+      + Self::C1C3R2 * R3;
+    let b2 = r4_b * C3 + C2 * r4_a + C1 * R3 + C1 * R2;
 
     let a0 = b0
-      + r4_b * c1c2c3r1r3
-      + c1c2c3r1 * r3r4_a
-      + r4_b * c1c2c3r1r2
-      + c1c2c3r1r2 * r4_a
-      + c1c2c3r1r2 * r3;
+      + r4_b * Self::C1C2C3R1R3
+      + Self::C1C2C3R1 * r3r4_a
+      + r4_b * Self::C1C2C3R1R2
+      + Self::C1C2C3R1R2 * r4_a
+      + Self::C1C2C3R1R2 * R3;
     let a1 = b1
-      + c2c3 * r2r4_a
-      + r4_b * c2c3r1
-      + c2c3r1 * r4_a
-      + c1c2r1 * r3
-      + c1c3r1 * r3
-      + c1c2r1 * r2
-      + c1c3r1 * r2
-      + c2c3r1 * r2;
-    let a2 = b2 + c3 * r2 + c2 * r1 + c3 * r1;
+      + Self::C2C3 * r2r4_a
+      + r4_b * Self::C2C3R1
+      + Self::C2C3R1 * r4_a
+      + Self::C1C2R1 * R3
+      + Self::C1C3R1 * R3
+      + Self::C1C2R1 * R2
+      + Self::C1C3R1 * R2
+      + Self::C2C3R1 * R2;
+    let a2 = b2 + C3 * R2 + C2 * R1 + C3 * R1;
 
     ([b0, b1, b2, 1.], [a0, a1, a2, 1.])
   }
